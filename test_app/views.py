21c468e30bbac1c1b917c7dafe0dbccfa315d268
Ullairi <maks.tih2005@gmail.com>
2025-11-08 21:52:26 +0100

    Add home_page view with username output

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page(request, user_name):
+    return HttpResponse(f"Hello, {user_name}")
