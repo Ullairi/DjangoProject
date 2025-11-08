7807b4920dcb69e33f17405d44770621bb87b183
Ullairi <maks.tih2005@gmail.com>
2025-11-08 21:51:48 +0100

    connected urls route from views.py

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from test_app.views import home_page
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # http://127.0.0.1:8000/admin/
+    path('<str:user_name>/', home_page),  # http://127.0.0.1:8000
 ]
