d7258a055986bfd913365c461347ec77af49d0a8
Ullairi <maks.tih2005@gmail.com>
2025-11-08 23:26:48 +0100

    Post, UserProfile, Category, Task, SubTask models registered in admin

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Post, UserProfile, Category, Task, SubTask
 
 admin.site.register(Book)
+admin.site.register(Post)
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(SubTask)
 
