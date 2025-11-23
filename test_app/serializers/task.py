7f8c718eaf76e31655741558a0dbd6ab0a9b264a
Ullairi <maks.tih2005@gmail.com>
2025-11-23 08:28:31 +0100

    Add TaskDetailSerializer and TaskCreateSerializer with deadline validation

--- test_app/serializers/task.py
+++ test_app/serializers/task.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from test_app.models import Task
+from test_app.serializers.subtask import SubTaskCreateSerializer
+from django.utils import timezone
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'status', 'deadline']
+        fields = ['id', 'title', 'description', 'status', 'deadline']
+
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    subtasks = SubTaskCreateSerializer(source='subtask_set', many=True, read_only=True)
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'deadline', 'created_at', 'subtasks']
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'status', 'deadline', 'categories']
+
+    def validate_deadline(self, value):
+        if value is not None and value < timezone.now():
+            raise serializers.ValidationError("Date can't be in past")
+        return value
