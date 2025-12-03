2101e156b08a64ed976b9b6c1b369f273abcd693
Ullairi <maks.tih2005@gmail.com>
2025-12-03 13:29:02 +0100

    Added Task,SubTask endpoints and Subtask filtering: Created GetTaskList Added filtering by day_of_week parametr Updated SubTaskListCreateView. Added Pagination with SubTaskPagiantion(5 subtasks per page) Added SubTaskFilter for filtering by main task title(task_title) and by status

--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,8 @@
 from django.core.serializers import serialize
 from django.http import HttpResponse
+from django.template.context_processors import request
 from rest_framework.decorators import api_view
+from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -10,6 +12,8 @@ from test_app.serializers.task import TaskCreateSerializer, TaskDetailSerializer
 from test_app.serializers.subtask import SubTaskCreateSerializer
 from django.utils import timezone
 from django.db.models import Count
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 
 @api_view(['POST'])
@@ -50,9 +54,13 @@ def stats_task(request):
 
 class SubTaskListCreateView(APIView):
     def get(self, request):
-        queryset = SubTask.objects.all()
-        subtask_dto = SubTaskCreateSerializer(queryset, many=True)
-        return Response(subtask_dto.data)
+        queryset = SubTask.objects.all().order_by("-created_at")
+
+        paginator = SubTaskPagination()
+        page = paginator.paginate_queryset(queryset, request)
+
+        serializer = SubTaskCreateSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         subtask_dto = SubTaskCreateSerializer(data=request.data)
@@ -95,6 +103,73 @@ class SubTaskDetailUpdateDeleteView(APIView):
 
         object.delete()
         return Response(status=204)
+
+class GetTaskList(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        queryset = Task.objects.all().order_by("-created_at")
+        day_of_week = self.request.query_params.get("day_of_week", None)
+
+        if day_of_week:
+            days = {
+                'monday': 2,
+                'tuesday': 3,
+                'wednesday': 4,
+                'thursday': 5,
+                'friday': 6,
+                'saturday': 7,
+                'sunday': 1,
+                'понедельник': 2,
+                'вторник': 3,
+                'среда': 4,
+                'четверг': 5,
+                'пятница': 6,
+                'суббота': 7,
+                'воскресенье': 1,
+            }
+            day_number = days.get(day_of_week.lower())
+            if day_number:
+                queryset = queryset.filter(deadline__week_day=day_number)
+        return queryset
+
+class SubTaskPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class SubTaskFilter(GenericAPIView):
+    serializer_class = SubTaskCreateSerializer
+    pagination_class = SubTaskPagination
+
+    def get_queryset(self):
+        queryset = SubTask.objects.all().order_by("-created_at")
+
+        task_title = self.request.query_params.get("task_title")
+        status_filter = self.request.query_params.get("status")
+
+        if task_title:
+            queryset = queryset.filter(task__title__icontains=task_title.strip())
+
+        if status_filter:
+            queryset = queryset.filter(status__iexact=status_filter.strip())
+
+        return queryset
+
+    def get(self,request):
+        queryset = self.get_queryset()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
 
 def home_page(request, user_name):
     return HttpResponse(f"Hello, {user_name}")
